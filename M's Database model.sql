285e2b5cbb6a07318b7c06b4a79f9e153ceba296
Manikandan2099 <98934300+Manikandan2099@users.noreply.github.com>
2022-03-08 19:45:52 +0530

    database updated

--- M's Database model.sql
+++ M's Database model.sql
@@ -1,37 +1,64 @@
-create database Banking
+--create database Banking
 
-create table Account_fields(
-title varchar,
-First_Name varchar,
-Last_Name varchar,
-Middle_Name varchar,
-Mobile_no varchar,
-Email_Id varchar,
-Aadhar_no varchar,
-DOB date,
-Residential_Address varchar,
-Permanent_Address varchar,
-Occupation_Type varchar,
-Source_of_income varchar,
-Gross_Annual_income money
+create table AccountFields(
+ReferenceID int identity(1,1) not null,
+Title	varchar(5) not null,
+FirstName	varchar(20) not null,
+LastName	varchar(20) not null,
+Mobileno	varchar(20) not null,
+MiddleName	varchar(20) ,
+emailID	varchar(40) not null,
+AadhaarCardNumber varchar(20) not null,
+DOB	date not null,
+ResidentialAddressLine1	varchar(120),
+ResidentialAddressLine2	varchar(120),
+ResidentialLandmark	varchar(120),
+ResidentialState	varchar(120) not null,
+ResidentialCity	varchar(120) not null,
+ResidentialPincode	varchar(120) not null,
+PermanentAddressLine1	varchar(120),
+PermanentAddressLine2	varchar(120),
+PermanentLandmark	varchar(120),
+PermanentState	varchar(120) not null,
+PermanentCity	varchar(120) not null,
+PermanentPincode	varchar(120) not null,
+Occupationdetails	varchar(120),
+OccupationType	varchar(120) ,
+Sourceofincome	money ,
+GrossAnnualIncome	int ,
 )
-
-Create Table NetBanking_Credentials
-( 
-User_Id int,
-Account_no int,
-User_name varchar
+Create Table NetBankingCredentials
+( 	
+UserID int primary key not null,
+NetBankingPassword	int not null,
+TransactionPassword	int not null
 )
-Create table Funds_Transfer
+create table DebitCardCredentials
 (
-Beneficiary_Type varchar,
-Beneficiary_Accno varchar,
-Beneficiary_IFSCcode varchar,
-Beneficiary_Name varchar
+DebitCardNumber int primary key not null,
+DebitCardExpiry date not null ,
+DebitCardCVV int not null
 )
-create table Transaction_History
+create table CreditCardCredentials
 (
-Date_of_Transaction datetime,
-Transferred_to varchar,
-Amount_Transferred money
+CreditCardNumber int primary key not null,
+CreditCardExpiry date not null,
+CreditCardCVV int not null
+)
+Create table ModeOfTransaction
+(	
+UserID int primary key not null,
+Modes	int not null,
+MaturityInstruction varchar
+)
+create table TransactionDetails
+(	
+TransactionReferenceID	int not null,
+AccountNumber	int not null,
+Name varchar not null,
+Nickname varchar,
+Transactionamount	money,
+TransactionDate	date,
+ModeofTransaction int not null,
+Remark	varchar
 )
