fb3f9609a7651a7f385d60a609ecf20dab700098
sunilskcj <sunilskcj@gmail.com>
2023-01-03 21:50:45 +0530

    client side added

---
+++ Client/src/app/net-banking/admin/requested-account-details/requested-account-details.component.ts
@@ -0,0 +1,69 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Observer } from 'rxjs';
+import { Account } from '../../Models/Account';
+import { ResponseModel } from '../../Models/ResponseModel';
+import { AccountService } from '../../services/account.service';
+import { AccCredentialService } from '../../services/credential.service';
+
+@Component({
+  selector: 'app-requested-account-details',
+  templateUrl: './requested-account-details.component.html',
+  styleUrls: ['./requested-account-details.component.css']
+})
+export class RequestedAccountDetailsComponent implements OnInit {
+
+  constructor(private _as : AccountService,private _cs : AccCredentialService, private route : ActivatedRoute ) { }
+  Account? : Account
+  errorMessage? : string
+  succes? : string
+  ngOnInit(): void {
+
+    let fromRoute =  this.route.snapshot.params['customerId'];
+    
+  console.log(fromRoute);
+  
+  this._as.getAccountbyIdAdmin(fromRoute).subscribe(
+    (Accounts : Account) => this.Account = Accounts )
+  }
+
+approve(){
+  const ObservableAccount: Observable<ResponseModel> = this._cs.addAccountCred(this.Account?.customerId)
+
+  const observerObj: Observer<ResponseModel> = {
+    next: (data: ResponseModel) => {
+      console.log(data)
+
+    },
+    error: (errresp: HttpErrorResponse) => {
+
+      console.log( errresp.message);
+      
+    },
+    complete: () => { this.succes = "Account is verified and Credentials are sent to their email id "}
+  };
+  ObservableAccount.subscribe(observerObj)
+
+}
+
+deny(){
+  const ObservableDeleteAccount: Observable<ResponseModel> = this._as.deleteAccount(this.Account?.customerId)
+
+  const observerObj: Observer<ResponseModel> = {
+    next: (data: ResponseModel) => {
+      console.log(data)
+
+    },
+    error: (errresp: HttpErrorResponse) => {
+
+      console.log( errresp.message);
+      
+    },
+    complete: () => {this.errorMessage = "This Account request is denied " }
+  };
+  ObservableDeleteAccount.subscribe(observerObj)
+}
+
+
+}
